7e60065c4344e8d0d7efb365f72972adeecfd668
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-12-05 17:04:42 +0600

    Details page animations

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
+//===> Slider Divs Reveals Animation start:
+
 //==> Variables:
 
 let controller;
 let sliderScene;
-let pageScene;
+let homePageScene;
+let detailsPageScene;
 
 //==> Functions:
 
@@ -14,9 +17,11 @@ function animateSliders() {
 	const headerNav = document.querySelector('.nav-header');
 
 	//Get the Sliders:
+
+	// For Home page:
 	const sliders = document.querySelectorAll('.slide');
 
-	// Looping over the Sliders:
+	// Looping over the Home page Sliders:
 	sliders.forEach((slider, index, sliders) => {
 		const img = slider.querySelector('img');
 		const revealImg = slider.querySelector('.reveal-img');
@@ -52,7 +57,7 @@ function animateSliders() {
 			})
 			.addTo(controller);
 
-		//-->> Page Animations Part:
+		//-->>Home Page Animations Part:
 
 		// Create page Timeline:
 		const pageTimeline = gsap.timeline();
@@ -72,7 +77,7 @@ function animateSliders() {
 		pageTimeline.fromTo(nextSlider, { y: '50%' }, { y: '0%' }, '-= 0.5');
 
 		// Create Page Scene:
-		pageScene = new ScrollMagic.Scene({
+		homePageScene = new ScrollMagic.Scene({
 			triggerElement: slider,
 			duration: '100%',
 			triggerHook: 0,
@@ -89,74 +94,141 @@ function animateSliders() {
 	});
 }
 
-animateSliders();
+//===> Home page Slider Divs Reveals Animation end <<===|
 
-//==> Cursor Animation by using gsap:
+//====> Drone Details page Scroll Animations start:
 
-// Selectors:
-const cursor = document.querySelector('.cursor');
-const follower = document.querySelector('.cursor-follower');
-const hoverables = document.querySelectorAll('img, .title, h1, a, h3');
+function droneDitailsAnimation() {
+	//Create Controller:
+	controller = new ScrollMagic.Controller();
 
-// set the initials values of 2D axis:
-var posX = 0,
-	posY = 0,
-	mouseX = 0,
-	mouseY = 0;
+	// Get the all slider from drone details page:
+	const sliders = document.querySelectorAll('.droneSlide-details');
 
-// Create Animation with gsap TweenMax:
-TweenMax.to({}, 0.016, {
-	repeat: -1,
-	onRepeat: function () {
-		posX += (mouseX - posX) / 9;
-		posY += (mouseY - posY) / 9;
+	//Looping over sliders:
+	sliders.forEach((slider, index, sliders) => {
+		const sliderTimeline = gsap.timeline({ defaults: { duration: 1 } });
 
-		// Set TweenMax:
-		TweenMax.set(follower, {
-			css: {
-				left: posX - 20,
-				top: posY - 20,
-			},
-		});
+		let nextDetailsSlider =
+			sliders.length - 1 === index ? 'end' : sliders[index + 1];
 
-		TweenMax.set(cursor, {
-			css: {
-				left: mouseX,
-				top: mouseY,
-			},
-		});
-	},
-});
+		// Get the next slider Imgs and text:
+		const nextImg = nextDetailsSlider.querySelector('img');
+		const nextText = nextDetailsSlider.querySelector('.dronecase-text');
 
-// Listeners with Functions:
-document.addEventListener('mousemove', (e) => {
-	mouseX = e.pageX;
-	mouseY = e.pageY;
-});
+		// Create Animations:
 
-hoverables.forEach((element) => {
-	element.addEventListener('mouseenter', () => {
-		cursor.classList.add('active');
-		follower.classList.add('active');
+		sliderTimeline.fromTo(slider, { opacity: 1 }, { opacity: 0 });
+		sliderTimeline.fromTo(
+			nextDetailsSlider,
+			{ opacity: 0 },
+			{ opacity: 1 },
+			'-= 1'
+		);
 
-		if (element.classList.contains('explore')) {
-			gsap.to('.title-swipe', 1, {
-				y: '0%',
+		sliderTimeline.fromTo(
+			nextImg,
+			{ x: '50%', y: '50%' },
+			{ x: '0%', y: '0%' }
+		);
+
+		sliderTimeline.fromTo(
+			nextText,
+			{ x: '-50%', y: '-50%' },
+			{ x: '0%', y: '0%' },
+			'-= 0.5'
+		);
+
+		// Create Scene:
+		detailsPageScene = new ScrollMagic.Scene({
+			triggerElement: slider,
+			triggerHook: 0,
+			duration: '100%',
+		})
+			.setPin(slider, { pushFollowers: false })
+			.setTween(sliderTimeline)
+			.addIndicators({
+				colorStart: 'lightblue',
+				colorTrigger: 'white',
+				name: 'page',
+				indent: 200,
+			})
+			.addTo(controller);
+	});
+}
+
+//====> Drone Details page Scroll Animations end.
+
+//==> Cursor Animation by using gsap start:
+
+function cursorAnimation() {
+	// Selectors:
+	const cursor = document.querySelector('.cursor');
+	const follower = document.querySelector('.cursor-follower');
+	const hoverables = document.querySelectorAll('img, .title, h1, a, h3');
+
+	// set the initials values of 2D axis:
+	var posX = 0,
+		posY = 0,
+		mouseX = 0,
+		mouseY = 0;
+
+	// Create Animation with gsap TweenMax:
+	TweenMax.to({}, 0.016, {
+		repeat: -1,
+		onRepeat: function () {
+			posX += (mouseX - posX) / 9;
+			posY += (mouseY - posY) / 9;
+
+			// Set TweenMax:
+			TweenMax.set(follower, {
+				css: {
+					left: posX - 20,
+					top: posY - 20,
+				},
 			});
-		}
+
+			TweenMax.set(cursor, {
+				css: {
+					left: mouseX,
+					top: mouseY,
+				},
+			});
+		},
 	});
 
-	element.addEventListener('mouseleave', () => {
-		cursor.classList.remove('active');
-		follower.classList.remove('active');
-
-		if (element.classList.contains('explore')) {
-			gsap.to('.title-swipe', 1, { y: '120%' });
-		}
+	// Listeners with Functions:
+	document.addEventListener('mousemove', (e) => {
+		mouseX = e.pageX;
+		mouseY = e.pageY;
 	});
-});
 
-//===> Burger Navbar Toggle:
+	hoverables.forEach((element) => {
+		element.addEventListener('mouseenter', () => {
+			cursor.classList.add('active');
+			follower.classList.add('active');
+
+			if (element.classList.contains('explore')) {
+				gsap.to('.title-swipe', 1, {
+					y: '0%',
+				});
+			}
+		});
+
+		element.addEventListener('mouseleave', () => {
+			cursor.classList.remove('active');
+			follower.classList.remove('active');
+
+			if (element.classList.contains('explore')) {
+				gsap.to('.title-swipe', 1, { y: '120%' });
+			}
+		});
+	});
+}
+
+//==> Cursor Animation by using gsap end <<==|
+
+//===> Burger Navbar Toggle start:
 
 // Selectors:
 const burgerNav = document.querySelector('.burger-nav');
@@ -201,6 +273,107 @@ function navToggler(e) {
 // Listeners:
 burgerNav.addEventListener('click', navToggler);
 
-const explore = document.querySelector('.explore');
+//===> Burger Navbar Toggle end <<==|
 
-console.log(explore);
+//===> Drones types Details Page start:
+
+// Page Transitions by using Barba JS:
+const logo = document.querySelector('#logo');
+
+barba.init({
+	// Pre and post work before Entering and Leaving any page:
+	views: [
+		{
+			namespace: 'home',
+			beforeEnter() {
+				animateSliders();
+				cursorAnimation();
+
+				logo.href = './index.html';
+			},
+			beforeLeave() {
+				sliderScene.destroy();
+				homePageScene.destroy();
+				controller.destroy();
+				cursorAnimation().destroy();
+			},
+		},
+		{
+			namespace: 'dronedetails',
+			beforeEnter() {
+				cursorAnimation();
+				droneDitailsAnimation();
+
+				logo.href = '../index.html';
+
+				gsap.fromTo(
+					'.nav-header',
+					1,
+					{ y: '-100%' },
+					{ y: '0%', ease: 'power2.inOut' }
+				);
+			},
+			beforeLeave() {
+				cursorAnimation().destroy();
+				controller.destroy();
+			},
+		},
+	],
+
+	//Create Animations on the pages:
+	transitions: [
+		{
+			//Page Leaving Animations:
+			leave({ current, next }) {
+				let done = this.async();
+
+				// FadeInOut Animation:
+				const fadeTimeline = gsap.timeline({
+					defaults: { ease: 'power2.inOut' },
+				});
+
+				fadeTimeline.fromTo(
+					current.container,
+					1,
+					{ opacity: 1 },
+					{ opacity: 0 }
+				);
+
+				// swipe animations:
+				fadeTimeline.fromTo(
+					'.swipe',
+					0.75,
+					{ x: '-100%' },
+					{ x: '0%', onComplete: done },
+					'-= 0.5'
+				);
+			},
+
+			//Page Entering Animations:
+			enter({ current, next }) {
+				let done = this.async();
+
+				// Scroll to top:
+				window.scrollTo(0, 0);
+
+				// FadeInOut Animation:
+				const fadeTimeline = gsap.timeline({
+					defaults: { ease: 'power2.inOut' },
+				});
+
+				fadeTimeline.fromTo(next.container, 1, { opacity: 0 }, { opacity: 1 });
+
+				// swipe animations:
+				fadeTimeline.fromTo(
+					'.swipe',
+					0.75,
+					{ x: '0%' },
+					{ x: '100%', stagger: 0.3, onComplete: done },
+					'-= 0.5'
+				);
+			},
+		},
+	],
+});
+
+//===> Drones types Details Page end <<==|
